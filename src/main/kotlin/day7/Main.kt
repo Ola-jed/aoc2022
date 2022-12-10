c361eb4782b0942d638a7b1fd1d6847bced6c8ed
olabijed@gmail.com <olabijed@gmail.com>
2022-12-10 19:13:42 +0100

    Part 2 done (day 7)

--- src/main/kotlin/day7/Main.kt
+++ src/main/kotlin/day7/Main.kt
@@ -3,6 +3,8 @@ package day7
 import java.io.File
 
 const val criticalFileSize = 100_000
+const val totalDiskSpace = 70000000
+const val requiredFreeSpace = 30000000
 
 fun main(args: Array<String> /* Pass the path to the input file as a command line argument */) {
     // Part 1
@@ -11,7 +13,16 @@ fun main(args: Array<String> /* Pass the path to the input file as a command lin
     val homeDir = parser.parse()
     val directories = homeDir.flat()
     val totalSize = directories.map { it.size() }.filter { it <= criticalFileSize }.sum()
-    println(totalSize)
+    println("Part 1 : $totalSize")
 
     // Part 2
+    val usedSpace = homeDir.size()
+    val freeSpace = totalDiskSpace - usedSpace
+    val spaceToFree = requiredFreeSpace - freeSpace
+    val deletableDirSize = findSmallestDeletableDirectorySize(directories, spaceToFree)
+    println("Part 2 : $deletableDirSize")
+}
+
+fun findSmallestDeletableDirectorySize(directories: List<Directory>, spaceToFree: Int): Int {
+    return directories.map { it.size() }.sortedBy { it }.first { it >= spaceToFree }
 }
